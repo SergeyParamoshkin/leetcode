4bd098f769e412b2b784006ef9e5cc0e8c7b0bf9
Sergey <serg3091@gmail.com>
2022-05-04 22:29:47 +0300

    perf(leetcode): :zap: add condiition when firts item > target

--- searchInsert/main.go
+++ searchInsert/main.go
@@ -2,6 +2,11 @@ package main
 
 func searchInsert(nums []int, target int) int {
 	pivot, left, right := 0, 0, len(nums)-1
+
+	if nums[0] > target {
+		return 0
+	}
+
 	for left <= right {
 		pivot = left + (right-left)/2
 		if nums[pivot] == target {
