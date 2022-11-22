1eb502c34b565fb1871bd5fea839c584f2a0a7c1
test <serg3091@mail.ru>
2022-11-22 11:12:10 +0300

    modify test package

--- power-of-four/power_of_four.go
+++ power-of-four/power_of_four.go
@@ -2,16 +2,16 @@ package poweroffour
 
 import "math"
 
-func isPowerOfFour(n int) bool {
-	if n <= 0 {
+func IsPowerOfFour(number int) bool {
+	if number <= 0 {
 		return false
 	}
 
-	if n == 1 {
+	if number == 1 {
 		return true
 	}
 
-	c := math.Sqrt(float64(n))
+	c := math.Sqrt(float64(number))
 
 	return int(c)%4 == 0
 }
--- power-of-four/power_of_four_test.go
+++ power-of-four/power_of_four_test.go
@@ -1,11 +1,18 @@
-package poweroffour
+package poweroffour_test
 
-import "testing"
+import (
+	"testing"
+
+	poweroffour "github.com/SergeyParamoshkin/leetcode/power-of-four"
+)
 
 func Test_isPowerOfFour(t *testing.T) {
+	t.Parallel()
+
 	type args struct {
 		n int
 	}
+
 	tests := []struct {
 		name string
 		args args
@@ -16,11 +23,15 @@ func Test_isPowerOfFour(t *testing.T) {
 		{name: "n=1", args: args{n: 1}, want: true},
 		{name: "n=-2147483648", args: args{n: -2147483648}, want: false},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := isPowerOfFour(tt.args.n); got != tt.want {
-				t.Errorf("isPowerOfFour() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	t.Run("test", func(t *testing.T) {
+		t.Parallel()
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := poweroffour.IsPowerOfFour(tt.args.n); got != tt.want {
+					t.Errorf("isPowerOfFour() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	})
 }
